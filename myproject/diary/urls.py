15ca204619026dffd90c0bce4f463b8bbb65d69c
naoki3 <cc2b6ebc72rnlw3@gmail.com>
2025-06-27 18:37:07 +0900

    create new diary app

---
+++ myproject/diary/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+app_name = "diary"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("page/create/", views.page_create, name="page_create"),
+    path("pages/", views.page_list, name="page_list"),
+    path("page/<uuid:id>/", views.page_detail, name="page_detail"),
+    path("page/<uuid:id>/update/", views.page_update, name="page_update"),
+    path("page/<uuid:id>/delete/", views.page_delete, name="page_delete"),
+]
